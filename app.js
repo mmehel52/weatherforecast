c4888ac5a6cf098a6fe80be76131cc69d3680f8f
mustafa mehel <mehelx5207@gmail.com>
2022-09-28 12:05:14 +0300

    last

--- app.js
+++ app.js
@@ -25,15 +25,21 @@ function getResult(cityName) {
 
 const renderError = () => {
   let content = document.querySelector(".content");
-  content.innerHTML = `
-    <img src="./12.jpg" alt="" style="width:500px" />
+  content.innerHTML += `<div class="contents"><div class="city">City not been found</div>
+    <img id= "err"src="./13.png" width=150px/>
   `;
+  if (content.childElementCount == 5) {
+    alert(`city count may be 4`);
+    content.innerHTML = "";
+  }
 };
 
 const displayResult = (result) => {
   let content = document.querySelector(".content");
-  content.innerHTML = `
- <div class="city">${result.name},${result.sys.country}</div>
+  content.innerHTML += `
+ <div class="contents"><div class="city">${result.name},${
+    result.sys.country
+  }</div>
         <div class="temp">${Math.round(result.main.temp)}°C</div>
         <img class="img" src="http://openweathermap.org/img/wn/${
           result.weather[0].icon
@@ -41,5 +47,9 @@ const displayResult = (result) => {
         <div class="desc">${result.weather[0].description}</div>
         <div class="minmax">${Math.round(result.main.temp_min)}°C/${Math.round(
     result.main.temp_max
-  )}°C</div>`;
+  )}°C</div></div>`;
+  if (content.childElementCount == 5) {
+    alert(`city count may be 4`);
+    content.innerHTML = "";
+  }
 };
